02cf2e939ad0eb9782a6022d4be3ac60288c52f2
Kristoffer Ahl <git@77dynamite.com>
2011-11-29 20:55:26 +0100

    Renamed Extensions to AssistExtensions and added tests for ToGlimpseSection.

---
+++ source/Glimpse.Test.Core/Plugin/Assist/AssistExtensionsTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Glimpse.Core.Plugin.Assist;
+using NUnit.Framework;
+
+namespace Glimpse.Test.Core.Plugin.Assist
+{
+    [TestFixture]
+	public class AssistExtensionsTest
+    {
+		[Test]
+		public void ToGlimpseSection_null_ShouldThrow()
+		{
+			object obj = null;
+
+			Assert.Throws<ArgumentNullException>(() => obj.ToGlimpseSection());
+		}
+
+        [Test]
+		public void ToGlimpseSection_NonGlimpseSectionObject_ShouldThrow()
+        {
+            object obj = new object();
+
+			Assert.Throws<InvalidOperationException>(() => obj.ToGlimpseSection());
+        }
+
+		[Test]
+		public void ToGlimpseSection_GlimseSection_ReturnsGlimpseSection()
+		{
+			object obj = new GlimpseSection();
+
+			var result = obj.ToGlimpseSection();
+
+			Assert.AreEqual(obj, result);
+		}
+
+		[Test]
+		public void ToGlimpseSection_GlimseSectionInstance_ReturnsGlimpseSection()
+		{
+			var section = new GlimpseSection();
+			object obj = section.Build();
+
+			var result = obj.ToGlimpseSection();
+
+			Assert.AreEqual(section, result);
+		}
+    }
+}
